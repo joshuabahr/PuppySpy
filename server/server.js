41a4f245c947a09e7721da3c1e0e2fb3e7fbb6f2
Joshua Bahr <joshuabahr73@gmail.com>
2018-07-29 18:08:57 -0700

    package.json server.js

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+const server = require('http').Server(app);
+
+const router = require('./router');
+const init = require('./init');
+
+const port = 3333;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '../static')));
+app.use('/api', router);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../static/index.html'));
+});
+
+init()
+  .then(() => {
+    server.listen(process.env.PORT || port, () => {
+      console.log(`app is listening on http://localhost:${port}`);
+    });
+  })
+  .catch(err => console.error('unable to connect to database ', err));

ab4fdc038ad0a2b0541a634d53a41a25c22c1592
Joshua Bahr <joshuabahr73@gmail.com>
2018-08-06 14:59:44 -0700

    updated init file

--- server/init.js
+++ server/init.js
@@ -1,10 +1,24 @@
 const db = require('./db/db');
 const Table = require('./models/tableModels');
 
-const init = () =>
-  db
+// to force refresh database, set dropDB to true
+const dropDB = false;
+
+
+// init function initiates connection to databases prior to server start
+const init = () => {
+
+  const refresh = dropDB ? { force: true } : null;
+  
+  return (
+    db
     .authenticate()
-    .then(() => Table.User.sync())
+    .then(() => Table.User.sync(refresh))
+    .then(() => Table.Cam.sync(refresh))
     .then(() => console.log('successfully synced with database'))
-    .catch(err => console.error('error syncing with database', err));
+    .catch(err => console.error('error syncing with database', err))
+  )
+  
+}
+
 module.exports = init;
